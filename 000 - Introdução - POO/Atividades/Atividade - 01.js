a17448ebd4d287e2b0ec8f8d539c183e6b3e3f04
Pequena Vênus <104568689+ElaneSz@users.noreply.github.com>
2022-05-29 15:49:36 -0300

    Update Atividade - 01.js

--- 000 - Introdução - POO/Atividades/Atividade - 01.js
+++ 000 - Introdução - POO/Atividades/Atividade - 01.js
@@ -1,23 +1,23 @@
 const canvas = document.createElement("canvas") 
- const context = canvas.getContext("2d") 
+const context = canvas.getContext("2d") 
   
- canvas.width = 800 
- canvas.height = 600 
+canvas.width = 800 
+canvas.height = 600 
   
- let y = 0 
- let barSize = 100 
- let directionY = 1 
+let y = 0 
+let barSize = 100 
+let directionY = 1 
   
- function mainLoop() { 
-     context.clearRect(0, 0, canvas.width, canvas.height) 
-     context.fillRect(10, y, 20, barSize) 
-     y += directionY 
-     if (y <= 0 || y >= canvas.height - barSize) { 
-         directionY *= -1 
-     } 
-     requestAnimationFrame(mainLoop) 
- } 
+function mainLoop() { 
+    context.clearRect(0, 0, canvas.width, canvas.height) 
+    context.fillRect(10, y, 20, barSize) 
+    y += directionY 
+    if (y <= 0 || y >= canvas.height - barSize) { 
+        directionY *= -1 
+    } 
+    requestAnimationFrame(mainLoop) 
+} 
   
- requestAnimationFrame(mainLoop) 
+requestAnimationFrame(mainLoop) 
   
- document.body.append(canvas)
+document.body.append(canvas)
